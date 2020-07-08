839f1d4cce388b2d6cafce858099f0892f8881d4
vagrant <vagrant@localhost.localdomain>
2020-07-08 04:21:48 +0000

    [Finish] 3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'd7a6b29fdc2976c507ce619ea43e45739e409e8c5d2dea33694750208884a67c4d8a8ce2e9e2d9804a4935ab29df90d4ccacaa8fed73876c524eeb0a50bca9a8'
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_07_054837) do
+ActiveRecord::Schema.define(version: 2020_07_07_112821) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_id"
   end
 
 end
